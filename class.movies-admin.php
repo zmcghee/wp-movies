8f537933b73e4fdb8fe1496f12bfbed7dcddc5dd
Zack McGhee <zack.mcghee@gmail.com>
2014-07-11 22:24:21 -0500

    Plugin admin screen

--- class.movies-admin.php
+++ class.movies-admin.php
@@ -2,7 +2,13 @@
 
 class Movies_Admin {
 	public static function init() {
-		echo "<!-- Movies Admin : Hello, world. -->";
+		add_action( 'admin_notices', array( 'Movies_Admin', 'display_notices' ) );
+	}
+	private static function menu() {
+	
+	}
+	private static function display_notices() {
+	    echo "<div id='notice' class='updated fade'><p>My Plugin is not configured yet. Please do it now.</p></div>\n";
 	}
 }
 
